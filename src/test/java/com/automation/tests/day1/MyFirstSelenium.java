21eac2711736cbec67dd973b52dfd78c51811a5a
Aierken712 <ershad0419@gmail.com>
2020-03-01 14:03:42 -0500

    Second commit

--- src/test/java/com/automation/tests/day1/MyFirstSelenium.java
+++ src/test/java/com/automation/tests/day1/MyFirstSelenium.java
@@ -3,7 +3,7 @@ package com.automation.tests.day1;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Practice {
+public class MyFirstSelenium {
 
     public static void main(String[] args) {
         //setup chromedriver
