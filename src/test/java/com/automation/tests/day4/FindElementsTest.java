026cad60e614727b74fa6a124b73a3c630df92ff
Aierken712 <ershad0419@gmail.com>
2020-03-04 20:45:34 -0500

    day4 selenium

---
+++ src/test/java/com/automation/tests/day4/FindElementsTest.java
@@ -0,0 +1,37 @@
+package com.automation.tests.day4;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.*;
+
+public class FindElementsTest {
+
+    public static void main(String[] args) throws Exception{
+
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/");
+
+        Thread.sleep(3000);
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        for (WebElement link : links) {
+            System.out.println(link.getText());
+            System.out.println(link.getAttribute("href"));
+            System.out.println();
+            link.click();
+            Thread.sleep(2000);
+            driver.navigate().back();
+        }
+
+
+
+
+        driver.quit();
+    }
+}
